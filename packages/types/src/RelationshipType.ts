886e1090d97d103b08d9e721f3c74cb6b9750dc2
josedaesbar <josedaesbar@gmail.com>
2021-07-16 21:23:06 -0500

    chore: mySQL edit table StudentTutors and Students

---
+++ packages/types/src/RelationshipType.ts
@@ -0,0 +1,12 @@
+enum RelationshipType {
+  PADRE = 'Padre',
+  MADRE = 'Madre',
+  ABUELO = 'Abuelo',
+  ABUELA = 'Abuela',
+  TIO = 'Tío',
+  TIA = 'Tía',
+  OTRO_FAMILIAR = 'OTRO FAMILIAR',
+  TUTOR = 'Tutor',
+}
+
+export default RelationshipType;
