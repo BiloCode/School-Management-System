68b9c672316d7311a9c6cfd9adddbcd39a370fef
Imanol Mayo <mayoimanol@gmail.com>
2021-07-22 15:53:04 -0500

    feat(server): add a authentication user services

---
+++ packages/server/src/__tests__/services/AuthenticationUser.test.ts
@@ -0,0 +1,16 @@
+import AuthenticationUser from '@services/mysql/AuthenticationUser';
+import mysqlConnection from '@config/mysql';
+import ComparePasswordWithBcrypt from '@services/ComparePassword';
+
+describe('User Services', () => {
+
+  test('Authentication User', async () => {
+    await mysqlConnection();
+    const response = 
+      await new AuthenticationUser(new ComparePasswordWithBcrypt())
+      .authentication('12345678', 'admin123');
+    
+    expect(response).toBe('aaaabbbbcccc'); // Correct credentials
+  })
+})
+
