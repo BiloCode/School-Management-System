42f8f7d89d34fb314011af979100c1505008a3ca
Jhony Vega <jhonyvegacuya24@gmail.com>
2021-08-30 00:26:49 -0500

    feat(web_application): perf components (#41)

--- packages/web_application/src/components/molecules/HomeworkItem/index.tsx
+++ packages/web_application/src/components/molecules/HomeworkItem/index.tsx
@@ -5,26 +5,27 @@ import Text from '../../atoms/Text';
 import { Fonts } from '../../../styles/font';
 import { TColor, TSize } from '../../../styles/text';
 import { HomeworkState } from '../../../styles/homework';
-import { Image } from 'react-native';
-
-const ChevronRight = require('../../../assets/icons/chevron-right.png');
-
+import { IconType } from '../../../styles/icons';
+import Icon from '../../atoms/Icon';
 
 type TProps = {
   course: string;
   date: string;
   state?: HomeworkState;
-  preview?: boolean;
 };
 
-const HomeworkItem = ({ course, date, state, preview }: TProps) => {
+const HomeworkItem = ({ course, date, state }: TProps) => {
   const textCourseColor =
     state === HomeworkState.lated ? TColor.RED : TColor.BLACK;
+  const iconState =
+    state === HomeworkState.lated
+      ? IconType.HOMEWORK_LATED
+      : IconType.HOMEWORK_OK;
 
   return (
     <S.Container>
       <View>
-        <Text>icon</Text>
+        <Icon icon={iconState} />
       </View>
       <S.DetailCourse>
         <Text weight={Fonts.BOLD} color={textCourseColor}>
@@ -32,11 +33,7 @@ const HomeworkItem = ({ course, date, state, preview }: TProps) => {
         </Text>
         <Text size={TSize.SMALL}>Se entregó el {date}</Text>
       </S.DetailCourse>
-      {preview && (
-        <View>
-          <Image source={ChevronRight} />
-        </View>
-      )}
+      <Icon icon={IconType.ARROW_RIGHT_BLACK} />
     </S.Container>
   );
 };
