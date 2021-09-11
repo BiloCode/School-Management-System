196dc15d6defe58a81d19d4d044090e4c483a966
Billy Alexander Paredes Aycho <73754175+BiloCode@users.noreply.github.com>
2021-09-11 17:20:30 -0500

    fix: eslint config fixed(#45)
    * fix(web_application): carousel images test fixed
    
    * fix: eslint config fixed

--- packages/web_application/src/__tests__/CarrouselImages.test.tsx
+++ packages/web_application/src/__tests__/CarrouselImages.test.tsx
@@ -6,7 +6,7 @@ import CarrouselImages from '../components/molecules/CarrouselImages';
 test('Renderizando flecha de retroceso', async () => {
   const images = [
     'https://static1-es.millenium.gg/articles/4/35/91/4/@/166681-honkai-impact-3rd-orig-2-article_image_bd-1.jpg',
-    'https://media.vandal.net/i/1280x720/4-2021/202142610103654_1.jpg.webp',
+    'https://media.vandal.net/i/1280x720/4-2021/202142610103654_1.jpg.webp'
   ];
 
   const screen = render(<CarrouselImages images={images} />);
@@ -19,5 +19,5 @@ test('Renderizando flecha de retroceso', async () => {
     ? screen.getByTestId('arrow-left')
     : undefined;
 
-  expect(arrowLeft).not.toBeUndefined;
+  expect(arrowLeft).not.toBeUndefined();
 });
